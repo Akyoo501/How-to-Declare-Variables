2e39a94045011e811373b9bcd33ca749f6f31548
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-01 10:52:40 +0300

    Add files via upload

---
+++ Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n1=55;
+	double n2=3.14;
+	float n3=33.65;
+	string name1="DAVID";
+	char name2[10]="AKYOO";
+	
+	cout<<n1<<endl;
+	cout<<n2<<endl;
+	cout<<n3<<endl;
+	cout<<name1<<endl;
+	cout<<name2<<endl;
+	
+	return 0;
+}
